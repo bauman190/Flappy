5808e9be34e520ee65a4adb83ac883a53d21eec2
Francisco <francisco.jonas@estudiantes.imagecampus.edu.ar>
2024-11-06 10:51:41 -0300

    add mouse complete structure and implementation to main loop

--- Flappy/src/entities/mouse.cpp
+++ Flappy/src/entities/mouse.cpp
@@ -1,1 +1,16 @@
 #include "mouse.h"
+
+namespace gameMouse
+{
+	Mouse CreateMouse(Mouse gameMouse)
+	{
+		gameMouse.mousePos = GetMousePosition();
+
+		return gameMouse;
+	}
+
+	void UpdateMousePos(Mouse& gameMouse)
+	{
+		gameMouse.mousePos = GetMousePosition();
+	}
+}
--- Flappy/src/entities/mouse.h
+++ Flappy/src/entities/mouse.h
@@ -1,1 +1,13 @@
-#pragma once 
+#pragma once
+#include "raylib.h"
+
+namespace gameMouse
+{
+	struct Mouse
+	{
+		Vector2 mousePos;
+	};
+
+	Mouse CreateMouse(Mouse gameMouse);
+	void UpdateMousePos(Mouse& gameMouse);
+}
